edc39f4d0fdf8b31451808b90830e1b6fb4dd232
Kauan <kauan.olibratoski@escola.pr.gov.br>
2025-05-21 18:19:01 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+const mensage = 'Professor, o senhor não pediu um código em js mas eu criei o arquivo e fiquei com preguiça de apagar, então fiz essa mensagem👍 ass: Kauan e Augusto'
+
+console.log(mensage)
